449af611d16a1fc07ba6da07d50a0344bd719473
samih1079 <sabbas79>
2017-10-04 10:22:50 +0300

    login

--- app/src/main/java/com/abbass/samih/samihcalculator/MainCalcActivity.java
+++ app/src/main/java/com/abbass/samih/samihcalculator/MainCalcActivity.java
@@ -2,12 +2,19 @@ package com.abbass.samih.samihcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainCalcActivity extends AppCompatActivity {
+
+    private EditText etNum1,edtNum2;
+    private Button btnPlus,btnMinois;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calc);
+        //kjhjkhsdhk
+        etNum1= (EditText) findViewById(R.id.etNum1);
     }
 }
